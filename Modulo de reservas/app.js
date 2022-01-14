e36f9f01f8e91ed3680a5374c2ce0f93333a3108
Dantex <daniel.kod@hotmail.com>
2022-01-13 22:44:20 -0500

    version reservas

---
+++ Modulo de reservas/app.js
@@ -0,0 +1,22 @@
+var amqp = require('amqp'); 
+
+function pub_and_sub() {
+  var exchange = conn.exchange(''); // get the default exchange
+  var queue = conn.queue('queue1', {}, function() { // create a queue
+    queue.subscribe(function(msg) { // subscribe to that queue
+      console.log(msg.body); // print new messages to the console
+    });
+
+    // publish a message
+    exchange.publish(queue.name, {body: 'Hello CloudAMQP!'}); 
+  });
+}
+
+var url = process.env.CLOUDAMQP_URL || "amqps://apfwqrdk:QfWRMKJpECkqHzz43MdFveLcQG3_YWFX@tiger.rmq.cloudamqp.com/apfwqrdk"; // default to localhost
+var implOpts = {
+  reconnect: true,
+  reconnectBackoffStrategy: 'linear', // or 'exponential'
+  reconnectBackoffTime: 500, // ms
+};
+var conn = amqp.createConnection({ url: url }, implOpts); // create the connection
+conn.on('ready', pub_and_sub); // when connected, call "pub_and_sub"
