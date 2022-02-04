48f8f96ee72b09cd97eefba5cb555d2aedb4110d
johan_mitma12 <avira365@hotmail.com>
2022-02-03 19:27:23 -0500

    interrup exception

--- Modulo de reservas/moduloReserva.py
+++ Modulo de reservas/moduloReserva.py
@@ -1,4 +1,5 @@
 import pika
+import sys
 import os
 import ast
 import db 
@@ -11,25 +12,34 @@ channel = connection.channel()
 
 #channel.queue_declare(queue='Reserva')
 
+def main():
 
-def callback(ch, method, properties, bodyMsg):
-    print('\n [x] Received :%r' % bodyMsg)    
-    msg = json.loads(bodyMsg)
-    print("Actualizando el stock de articulos en la bd...")
+    def callback(ch, method, properties, bodyMsg):
+        print('\n [x] Received :%r' % bodyMsg)    
+        msg = json.loads(bodyMsg)
+        print("Actualizando el stock de articulos en la bd...")
 
-    database = db.DataBase()
-    #actualizar las cantidades en la BD de los productos solicitados
-    for articulo in msg['detalles']:
-        cantActual = database.seleccionarCantidad(articulo['cod_articulo'])
-        database.actualizarCantidad(articulo['cod_articulo'], cantActual-articulo['cantidad'])
-    database.close()
+        database = db.DataBase()
+        #actualizar las cantidades en la BD de los productos solicitados
+        for articulo in msg['detalles']:
+            cantActual = database.seleccionarCantidad(articulo['cod_articulo'])
+            database.actualizarCantidad(articulo['cod_articulo'], cantActual-articulo['cantidad'])
+        database.close()
 
-    #crear la cola Factura y enviar el mensaje a la cola
-    #channel.queue_declare(queue='Factura')
-    channel.basic_publish(exchange='exchange_procesosnegocio', routing_key='key_cola_facturacion', body=bodyMsg)
-    print("Enviado a modulo de FACTURACIÓN...!")
+        #crear la cola Factura y enviar el mensaje a la cola
+        #channel.queue_declare(queue='Factura')
+        channel.basic_publish(exchange='exchange_procesosnegocio', routing_key='key_cola_facturacion', body=bodyMsg)
+        print("Enviado a modulo de FACTURACIÓN...!")
 
-channel.basic_consume(queue='cola_reserva', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='cola_reserva', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press ctrl + c')
-channel.start_consuming()
+    print(' [*] Waiting for messages. To exit press ctrl + c')
+    channel.start_consuming()
+    connection.close()
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Saliendo del programa")
+        sys.exit(0)
